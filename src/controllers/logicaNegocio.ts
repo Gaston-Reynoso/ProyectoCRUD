f9693ad97d34506cd14595be0a3f38477166c391
Gaston <gastonn.reynoso@gmail.com>
2025-04-22 10:08:56 -0300

    Delete and Update

--- src/controllers/logicaNegocio.ts
+++ src/controllers/logicaNegocio.ts
@@ -25,6 +25,9 @@ const bookSchema = new Schema<bookInterface>({
 bookSchema.set("strict", true) //Para evitar que valide otras props que no esten en el schema.
 const Book = mongoose.model<bookInterface>("book", bookSchema)//Para acceder a los libros debo de pasar por el esquema.
 
+
+
+//Crear Libro
 export const createBook = async (newBook: object) => {
     try {
       const book: bookInterface = new Book(newBook)
@@ -36,7 +39,8 @@ export const createBook = async (newBook: object) => {
     }
   }
 
-  export const getBook = async () => {
+  //Mostrar Libro
+  export const getBooks = async () => {
     try {
       const books = await Book.find()
       console.log(books)
@@ -46,7 +50,8 @@ export const createBook = async (newBook: object) => {
     }
   }
 
-  const getBookById = async (id: string) => {
+  //Mostrar libro por ID
+  export const getBookById = async (id: string) => {
     try {
       const book = await Book.findById(id);
   
@@ -60,3 +65,35 @@ export const createBook = async (newBook: object) => {
     }
   };
 
+
+  //Actualizar libro
+  export const updateBook = async (id: string, body: object) => {
+    try {
+      const updateBook = await Book.findByIdAndUpdate(id, body, { new: true }) //Esto se hace en MONGO ({ new: true } me da la version actualizada del objeto)
+      if (!updateBook) {
+        return { sucess: false, message: "Libro no existente" }
+      } else {
+        return { sucess: true, data: updateBook, message: "Libro actualizado" }
+      }
+    } catch (error: any) {
+      return { sucess: false, message: error.message }
+    }
+  }
+
+
+  //Eliminar libro
+  export const deleteBook = async (id: string) => {
+    try {
+      const deletedBook = await Book.findByIdAndDelete(id) 
+      if (!deletedBook) {
+        return { sucess: false, message: "Libro no existente" }
+      } else {
+        return { sucess: true, data: deletedBook, message: "Libro actualizado" }
+      }
+    } catch (error: any) {
+      return { sucess: false, message: error.message }
+    }
+  }
+
+  getBooks();
+  getBookById("68065331766fbffd9969bafc");
